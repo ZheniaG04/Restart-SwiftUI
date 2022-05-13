e39af4f7d201b3dd3b75ba60fedc75d94aadaed0
Yevhenii Huts <yevhenii.huts@globallogic.com>
2022-05-13 23:39:05 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Yevhenii Huts on 13.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
